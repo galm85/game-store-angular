f4bbf1c9af7a38f53e60aeb34d16c6873c06e996
Gal <galg@elad.co.il>
2025-04-28 17:12:37 +0300

    create header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AccessoriesComponent } from './pages/accessories/accessories.component';
 
 export const routes: Routes = [
-  {
-    path:'',
-    component:HomepageComponent
-  }
+  {path:'',component:HomepageComponent},
+  {path:'about',component:AboutComponent},
+  {path:'accessories',component:AccessoriesComponent},
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
+type navigationLink = {label:string,link:string}
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  navLinks:navigationLink[] = [
+    {label:'About',link:'about'},
+    {label:'Games',link:'games'},
+    {label:'Accessories',link:'accessories'},
+    {label:'Contact Us',link:'contact-us'},
+  ]
 
 }
