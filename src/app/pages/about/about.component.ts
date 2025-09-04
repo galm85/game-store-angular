77336313c3d59f60b95e49ebdf9ffcdec28baec6
Gal <galm85@gmail.com>
2025-09-04 14:11:24 +0300

    create about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { BreadcrumbsComponent } from "../../components/ui/breadcrumbs/breadcrumbs.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [BreadcrumbsComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+
 
 }
